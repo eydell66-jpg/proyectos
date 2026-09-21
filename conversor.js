61d65d91326eaf562b4a833fd06487a5bae35181
Fernanda <eydell66@gmail.om>
2026-09-21 21:22:28 +0200

    funcion javaconversor

--- conversor.js
+++ conversor.js
@@ -1,6 +1,15 @@
-const nombre = "Tueste Norte";
-let contador = 0;
 
-function precioConIva(precio) {
-    return precio * 1.21;
-}
+const selectCafe = document.getElementById('cafe');
+const inputCantidad = document.getElementById('cantidad');
+const inputTotal = document.getElementById('total');
+
+// funcion que realiza la operación matemática //
+const precio = parseFloat(selectCafe.value) || 0;
+const cantidad = parseInt(inputCantidad.value) || 0;
+
+// operacion con dos decimales
+const totalPago = precio * cantidad;
+
+// Escuchar cambios en los elementos para calculo en tiempo real //
+selectCafe.addEventListener('change', calcularTotal);
+inputCantidad.addEventListener('input', calcularTotal);
