96a357256ea88caee9820ad64ccd39475c76e64d
Fernanda <eydell66@gmail.om>
2026-09-22 19:56:39 +0200

    mensaje en caso de 0

--- conversor.js
+++ conversor.js
@@ -1,23 +1,30 @@
-
 const selectCafe = document.getElementById('cafe');
 const inputCantidad = document.getElementById('cantidad');
 const inputResultado = document.getElementById('resultado')
 const botonCalcular = document.getElementById('botoncalcular');
+
 
 // Escuchar el clic del boton //
 botonCalcular.addEventListener('click', function() {
 
 // conversion de value a un numero //    
 const precio = parseFloat(selectCafe.value) || 0;
-
 const cantidad = parseInt(inputCantidad.value) || 0;
 
-// operacion con dos decimales
-const totalPago = precio * cantidad;
+if (cantidad === 0){
+    
+    inputResultado.value = "Introduce una cantidad valida";
+} else {
+   // operacion con dos decimales
+    const totalPago = precio * cantidad;
+   inputResultado.value = totalPago.toFixed(2); 
 
-// Mostramos el resultado en la casilla resultado //
-inputResultado.value = totalPago.toFixed(2);
+
+
+}
+
+
+
 
 });
-
 
